067c173b63b4c1945f41b4c10ecddcc37d980247
Agustín Alexander Turchetti Sahajdachy <69662336+agustur@users.noreply.github.com>
2024-05-30 22:43:03 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("code-card");
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
+    }
+
+    $('.prev').click(function () {
+        plusSlides(-1);
+    });
+
+    $('.next').click(function () {
+        plusSlides(1);
+    });
+
+    $('.code-card').on('click', function () {
+        plusSlides(1);
+    });
+
+});
